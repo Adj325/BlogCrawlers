40dd02e380faab1e73d23d60b69f739e6b5b80a6
wujiechun <wujiechun@gomo.com>
2021-06-06 21:14:31 +0800

    代码调整

--- @博客爬取.py
+++ @博客爬取.py
@@ -1,11 +1,12 @@
 import re
 import os
+
 try:
     import requests
 except:
     os.system('pip install requests')
     import requests
-    
+
 try:
     from markdownify import markdownify
 except:
@@ -36,6 +37,8 @@ headers = {
 }
 debug = True
 soup = None
+
+
 class Blog:
     def __init__(self):
         self.title = ''
@@ -60,16 +63,16 @@ class BlogCrawler:
     def run(self):
         blog_url = input('博客网址: ').strip()
         # blog_url = 'https://www.jianshu.com/p/215600b11413'
-        #blog_url = 'https://blog.csdn.net/hellozpc/article/details/106861972'
+        # blog_url = 'https://blog.csdn.net/hellozpc/article/details/106861972'
         # blog_url = 'https://www.cnblogs.com/wanlei/p/10650325.html'
         # blog_url = 'https://segmentfault.com/a/1190000011105644'
         # blog_url = 'https://blog.51cto.com/yht1990/2503819'
-        
+
         # blog_url = 'https://zhuanlan.zhihu.com/p/28375308'
         # blog_url = 'https://mp.weixin.qq.com/s/-zKO0TZPqhCB6nyuUyADUw'
         # blog_url = 'https://www.jb51.net/article/174387.htm'
-        #blog_url = 'https://juejin.im/post/5ef7328cf265da22a8513da2'
-        
+        # blog_url = 'https://juejin.im/post/5ef7328cf265da22a8513da2'
+
         blog_url_host = self.get_host_from_url(blog_url)
         print('博客网站:', blog_url_host)
         blog = Blog()
@@ -87,18 +90,13 @@ class BlogCrawler:
         encoding_type = self.get_html_chatset(r.text)
         # 设置编码格式
         r.encoding = encoding_type
-        #print('编码格式:', r.encoding)
+        # print('编码格式:', r.encoding)
         # 获取文本内容
         html = r.text
-        #html = open('temp - 副本.html', encoding='utf-8').read()
         soup = BeautifulSoup(html, 'lxml')
-        if False:
-            # 增加代码标签
-            html = re.sub('<code.*?>', '<code>```\n', html)
-            html = re.sub('</code>', '\n```</code>', html)
         with open('temp.html', 'w', encoding='utf-8') as f:
             f.write(html)
-            
+
         # 正则获取标题
         title_bs_args = rule['title_bs_args']
         titles = self.get_content_by_bs_args(soup, title_bs_args, 'title')
@@ -113,64 +111,54 @@ class BlogCrawler:
         # 提取正文内容
         content_bs_args = rule['content_bs_args']
         content = self.get_content_by_bs_args(soup, content_bs_args)
-        
-        content = '<h1><a href="{}">{}</a></h1><br><br>'.format(blog.url, blog.title) + content
+
         for src, dst in rule['content_replaces']:
             content = re.sub(src, dst, content)
         blog.content = content
         # print('正文:', content)
 
         # 转换为 MD
-        # md_content = Tomd(content).markdown
-        # content = re.sub('<a id=".*?"></a>', '', content)
-        # code_tag_replace_text = 'magic&%sd我他喵真帅sf*codestart*-sdfa*'
-        # content = content.replace('<code>', code_tag_replace_text).replace('</code>', code_tag_replace_text)
-        #text_maker = ht.HTML2Text()
-        content1 = '''<pre><code class="java">@Component
-public class MyApplicationListener implements ApplicationListener&lt;ApplicationEvent&gt; {
- 
-@Override
-public void onApplicationEvent(ApplicationEvent event) {
-  System.out.println("事件触发："+event.getClass().getName());
-}</code></pre><p>然后启动自己的springboot项目：</p><pre><code class="java">@SpringBootApplication
-public class ApplicationListenerDemoApplication {
-
-}
-</code>'''
-        #md_content = text_maker.handle(content)
         md_content = markdownify(content, heading_style="ATX")
         if debug:
-            print('===========【原始】============\n'+md_content)
+            print('===========【原始】============\n' + md_content)
 
         # 正则替换
         for src, dst in rule['md_replaces']:
             md_content = re.sub(src, dst, md_content)
         if debug:
-            print('===========【正则替换】============\n'+md_content)
+            print('===========【正则替换】============\n' + md_content)
         # 加空格
         md_content = pangu.spacing_text(md_content)
         if debug:
-            print('===========【加空格】============\n'+md_content)
+            print('===========【加空格】============\n' + md_content)
 
         # 修复 ```中文```
         md_content = md_content.replace('```', '\n\n```\n\n')
         if debug:
-            print('===========【修复 ```中文```】============\n'+md_content)
+            print('===========【修复 ```中文```】============\n' + md_content)
 
         # 修复pangu带来的md格式错误
         md_content = self.fix_mdfile_bold_format(md_content)
 
         # 修复不严格的代码片段
-        #md_content = self.fix_mdfile_code_format(md_content)
+        # md_content = self.fix_mdfile_code_format(md_content)
         # 修复断行
         md_content = self.fix_mdfile_wrong_line_break(md_content)
-
+        # 去除空白行
+        md_content = self.fix_code_format_v2(md_content)
         # 修复代码方法 () 前的多余空格
         md_content = self.fix_mdfile_wrong_spacing(md_content)
-        # 去除空白行
+        md_content = self.fix_code_format_v2(md_content)
+        md_content = self.remove_blank_line(md_content)
+        md_content = md_content.replace('```\n\n', '```\n').replace('\n\n```', '\n```')
+        with open("blogs" + os.sep + title.replace('\n', '').replace('*', '').replace('/', ' ') + '.md', 'w',
+                  encoding='utf-8') as f:
+            md_content = '# [{}]({})\n\n'.format(blog.title, blog.url) + md_content
+            f.write(md_content)
+
+    @staticmethod
+    def remove_blank_line(md_content):
         md_content = re.sub('\n[  ]+\n', '\n\n', md_content)
-        # 解决 {System 问题
-        md_content = md_content.replace('{', '{\n').replace('}', '\n}')
         # 去空行
         md_content = md_content.replace('\r', '')
         while ' \n' in md_content:
@@ -178,14 +166,21 @@ public class ApplicationListenerDemoApplication {
         while '\n\n\n' in md_content:
             md_content = md_content.replace('\n\n\n', '\n\n')
         if debug:
-            print('===========【去空行】============\n'+md_content)
-        md_content = md_content.replace('{\n\n', '{\n').replace('\n\n}', '\n}')
-        md_content = md_content.replace('```\n\n', '```\n').replace('\n\n```', '\n```')
-        with open("blogs" + os.sep + title.replace('\n', '').replace('*', '').replace('/', ' ') + '.md', 'w', encoding='utf-8') as f:
-            f.write(md_content)
-        pass
+            print('===========【去空行】============\n' + md_content)
+        return md_content
 
-        
+    '''
+    修复{System.out
+    }
+    '''
+    @staticmethod
+    def fix_code_format_v2(md_content):
+        md_content = md_content.replace('{', '{\n').replace('}', '\n}')
+        md_content = md_content.replace('{\n}', '{}')
+        md_content = md_content.replace('{\n\n', '{\n').replace('\n\n}', '\n}')
+        md_content = md_content.replace('{\n\n', '{\n').replace('\n\n}', '\n}')
+        return md_content
+
     @staticmethod
     def fix_mdfile_wrong_spacing(md_content):
         words = re.findall('(\w+) \(', md_content)
@@ -208,7 +203,7 @@ public class ApplicationListenerDemoApplication {
         # 连接含有 - 会断行
         urls = re.findall('://(.*?)-', md_content)
         for url in urls:
-            md_content = md_content.replace(url+'-\n', url+'-')
+            md_content = md_content.replace(url + '-\n', url + '-')
         else:
             return md_content
 
@@ -231,12 +226,12 @@ public class ApplicationListenerDemoApplication {
     @staticmethod
     def get_content_by_bs_args(soup, bs_args_list, type="content"):
         for bs_args in bs_args_list:
-            soup = soup.find(bs_args['name'], attrs = bs_args['attrs'])
+            soup = soup.find(bs_args['name'], attrs=bs_args['attrs'])
         if not type == "content":
             return soup.get_text()
         else:
             return str(soup)
-            
+
     @staticmethod
     def get_html_chatset(html):
         charset = 'utf-8'
@@ -247,7 +242,7 @@ public class ApplicationListenerDemoApplication {
     def fix_mdfile_bold_format(text):
         question_regex = ['\*\* (.*?) \*\*', '\* (.*?) \*']
         fixed_template = ['**{}**', '*{}*']
-        assert(len(question_regex) == len(fixed_template))
+        assert (len(question_regex) == len(fixed_template))
         for index in range(len(fixed_template)):
             match_list = re.finditer(question_regex[index], text)
             for m in match_list:
@@ -262,19 +257,22 @@ public class ApplicationListenerDemoApplication {
         cur_lines_count = 0
         while cur_lines_count < len(lines):
             if code_start_line_count == -1:
-                if lines[cur_lines_count] == '' and cur_lines_count < len(lines) - 1 and lines[cur_lines_count + 1][0:4] == '    ':
+                if lines[cur_lines_count] == '' and cur_lines_count < len(lines) - 1 and lines[cur_lines_count + 1][
+                                                                                         0:4] == '    ':
                     # 代码开始
                     code_start_line_count = cur_lines_count
                 cur_lines_count += 1
             else:
                 # 代码已经开始了
                 if lines[cur_lines_count][0:4] == '    ':
-                    if cur_lines_count < len(lines) - 2 and lines[cur_lines_count + 1] == '' and not lines[cur_lines_count + 2][0:4] == '    ':
+                    if cur_lines_count < len(lines) - 2 and lines[cur_lines_count + 1] == '' and not lines[
+                                                                                                                 cur_lines_count + 2][
+                                                                                                     0:4] == '    ':
                         # 代码结束的标志1
                         code_end_line_count = cur_lines_count + 1
                         # 这里+1 与 下边的+1不冲突
                         cur_lines_count += 1
-                        
+
                         lines[code_start_line_count] = '```'
                         lines[code_end_line_count] = '```'
                         for i in range(code_start_line_count + 1, code_end_line_count):
@@ -292,6 +290,7 @@ public class ApplicationListenerDemoApplication {
                 else:
                     cur_lines_count += 1
         return '\n'.join(lines)
+
 
 debug = False
 while True:
