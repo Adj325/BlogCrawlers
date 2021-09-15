52b0209e754a56a89b52f49c5883a446f623f3ea
wujiechun <wujiechun@gomo.com>
2021-09-15 21:21:42 +0800

    '更改默认处理逻辑'

--- ImageCrawler.py
+++ ImageCrawler.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     print('当前目录: ' + os.getcwd() + '\n')
     print('    1: 处理当前目录下的所有 markdown 文件')
     print('    2: 处理当前目录及其子目录下的所有 markdown 文件')
-    print('enter: 处理 blogs 目录下的所有 markdown 文件')
     choice = input('输入：')
     print()
     markdown_file_paths = []
@@ -19,6 +18,10 @@ if __name__ == '__main__':
         blogs_dir_path = os.path.join(os.getcwd(), "blogs")
         if os.path.exists(blogs_dir_path):
             markdown_file_paths = get_markdown_files(blogs_dir_path)
+        else:
+            markdown_file_paths = [os.path.join(os.getcwd(), file_path)
+                               for file_path in os.listdir(os.getcwd())
+                               if '.md' in file_path and '.old' not in file_path]
     input('\n--回车后，开始处理--')
     for markdown_file_path in markdown_file_paths:
         download_images_for_markdown_file(markdown_file_path, is_backup_old_file)
